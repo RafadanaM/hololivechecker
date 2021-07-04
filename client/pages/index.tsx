af438eaedcd32ce7e1da93903cb45e716c83321a
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2021-07-04 08:25:14 +0700

    remove unused functions

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,36 +1,11 @@
-import { Box, Button, CircularProgress, Tab, Tabs } from "@material-ui/core";
-import { StylesProvider, withStyles } from "@material-ui/styles";
+import { Button, CircularProgress } from "@material-ui/core";
 import { AxiosError } from "axios";
-import { GetServerSideProps, GetStaticProps } from "next";
+import { GetServerSideProps } from "next";
 import React, { useState } from "react";
 import axios from "../axios/axios";
 import Card from "../components/Card";
-import TabItem from "../components/TabItem";
 import { HoloMember, MembersResponse } from "../interface";
 import classes from "../styles/Index.module.css";
-
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: any;
-  value: any;
-}
-
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-      className={classes.tabContent}
-    >
-      {value === index && <div className={classes.box}>{children}</div>}
-    </div>
-  );
-}
 
 export interface HomePageProps {
   members: MembersResponse;
@@ -38,13 +13,7 @@ export interface HomePageProps {
 }
 
 function HomePage({ members, error }: HomePageProps) {
-  const [page, setPage] = useState(0);
   const [filter, setFilter] = useState("all");
-
-  const handlePageChange = (event: React.ChangeEvent<{}>, newValue: number) => {
-    setPage(newValue);
-  };
-
   const changeFilterHandler = (gen: string) => {
     console.log("change filter to " + gen);
 
