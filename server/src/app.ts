09c985474fed7a64c65ea5a3bd8244396cd519a3
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2021-11-20 08:35:06 +0700

    test husky

--- server/src/app.ts
+++ server/src/app.ts
@@ -36,12 +36,12 @@ export class App {
     });
   }
 
-  private initErrorHandling() {
-    this.app.use(errorMiddleware);
-  }
-
   private initRouteNotFound() {
     this.app.use(NotFoundMiddleware);
+  }
+
+  private initErrorHandling() {
+    this.app.use(errorMiddleware);
   }
 
   private initCron() {
