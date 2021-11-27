66c6220b54c780e9a9d8897acce955eec418b8c4
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2021-11-27 14:52:15 +0700

    fix env validation

--- server/src/ormconfig.ts
+++ server/src/ormconfig.ts
@@ -9,6 +9,7 @@ const ormconfig: ConnectionOptions = {
   database: process.env.DB_NAME,
   entities: [__dirname + "/**/*.entity{.ts,.js}"],
   migrations: [__dirname + "/migrations/*{.ts,.js}"],
+  ssl: true,
   cli: {
     migrationsDir: "src/migrations",
   },
