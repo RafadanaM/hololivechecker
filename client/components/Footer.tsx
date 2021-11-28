399b5909a37c9e29ac73fce5eaaf64187abc562e
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2021-11-28 17:49:31 +0700

    add footer item

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,9 +1,40 @@
 import React from "react";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <footer className="mt-auto w-full bg-primary text-center text-white">
-      <p className="text-xl">This is footer</p>
+      <ul>
+        <li>
+          <a
+            href="https://github.com/RafadanaM/hololivechecker"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              height={32}
+              width={32}
+              src="/github.png"
+              alt="github.com/RafadanaM/hololivechecker"
+            />
+          </a>
+        </li>
+
+        <li>
+          <a
+            href="mailto:RafadanaM@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              height={32}
+              width={32}
+              src="/email.png"
+              alt="RafadanaM@gmail.com"
+            />
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 };
