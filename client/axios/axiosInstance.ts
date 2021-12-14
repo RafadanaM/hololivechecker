7b744fe282a18a28a4b099c43968115f56c15987
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2021-12-14 10:48:38 +0700

    Update axiosInstance.ts

--- client/axios/axiosInstance.ts
+++ client/axios/axiosInstance.ts
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:5000/api",
-  headers: {
-    "content-type": "application/json",
-  },
   responseType: "json",
   withCredentials: true,
 });
